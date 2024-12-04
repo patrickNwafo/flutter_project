60493ce453668089c31e5311465f852ea5ab9be8
ricky <patrickchinwafor@gmail.com>
2024-12-04 16:02:46 +0100

    Ticket View part 4

--- lib/screens/tickets/ticket_screen.dart
+++ lib/screens/tickets/ticket_screen.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/app_jason.dart';
@@ -18,32 +19,46 @@ class TicketScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
         children: [
-          const SizedBox(height: 40,),
-          Text("Tickets", style: AppStyles.headLineStyle1,),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Tickets",
+            style: AppStyles.headLineStyle1,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
           const AppTicketTabs(
             firstTab: "Upcoming",
             secondTab: "Previous",
           ),
-          const SizedBox(height: 20,),
-          Container(
-            padding: const EdgeInsets.only(left: 16),
-              child: TicketView(ticket: ticketList[0],isColor: true,)
+          const SizedBox(
+            height: 20,
           ),
-          const SizedBox(height: 1,),
+          //white ticket
+          Container(
+              padding: const EdgeInsets.only(left: 16),
+              child: TicketView(
+                ticket: ticketList[0],
+                isColor: true,
+              )),
+          const SizedBox(
+            height: 1,
+          ),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 15),
             padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
             color: AppStyles.ticketColor,
-            child:  Column(
+            child: Column(
               children: [
                 const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     AppColumnTextLayout(
-                        topText: "Flutter DB",
-                        bottomText: "Passenger",
-                        alignment: CrossAxisAlignment.start,
+                      topText: "Flutter DB",
+                      bottomText: "Passenger",
+                      alignment: CrossAxisAlignment.start,
                       isColor: true,
                     ),
                     AppColumnTextLayout(
@@ -54,9 +69,17 @@ class TicketScreen extends StatelessWidget {
                     )
                   ],
                 ),
-                const SizedBox(height: 20,),
-                const AppLayoutBuilderWidget(randomDivider: 15, width: 5, isColor: false,),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
                 const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -74,9 +97,17 @@ class TicketScreen extends StatelessWidget {
                     )
                   ],
                 ),
-                const SizedBox(height: 20,),
-                const AppLayoutBuilderWidget(randomDivider: 15, width: 5, isColor: false,),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -84,12 +115,23 @@ class TicketScreen extends StatelessWidget {
                       children: [
                         Row(
                           children: [
-                            Image.asset(AppMedia.visaCard, scale: 11,),
-                            Text(" *** 2462", style: AppStyles.headLineStyle2,)
+                            Image.asset(
+                              AppMedia.visaCard,
+                              scale: 11,
+                            ),
+                            Text(
+                              " *** 2462",
+                              style: AppStyles.headLineStyle2,
+                            )
                           ],
                         ),
-                        const SizedBox(height: 5,),
-                        Text("Payment Method", style: AppStyles.headLineStyle4,)
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Payment Method",
+                          style: AppStyles.headLineStyle4,
+                        )
                       ],
                     ),
                     const AppColumnTextLayout(
@@ -102,7 +144,39 @@ class TicketScreen extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          const SizedBox(height: 1,),
+          // Bottom of the ticket detail section
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            padding: EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+                color: AppStyles.ticketColor,
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(21),
+                    bottomLeft: Radius.circular(21))),
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: BarcodeWidget(
+                  height: 70,
+                  barcode: Barcode.code128(),
+                  data: 'https://port-one-opal.vercel.app/',
+                  drawText: false,
+                  color: AppStyles.textColor,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          //colorful ticket
+          Container(
+              padding: const EdgeInsets.only(left: 16),
+              child: TicketView(
+                ticket: ticketList[0],
+              )),
         ],
       ),
     );
