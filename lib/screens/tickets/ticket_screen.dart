a964b81db59e57caa07c19ef922c2405ce5ab584
ricky <patrickchinwafor@gmail.com>
2024-12-04 16:33:40 +0100

    Ticket View part 5

--- lib/screens/tickets/ticket_screen.dart
+++ lib/screens/tickets/ticket_screen.dart
@@ -6,6 +6,7 @@ import 'package:ticket_app/base/widgets/app_column_text_layout.dart';
 import 'package:ticket_app/base/widgets/app_layoutbuilder_widget.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
+import 'package:ticket_app/screens/tickets/widgets/ticket_position_circle.dart';
 
 import '../../base/res/media.dart';
 
@@ -16,167 +17,173 @@ class TicketScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      body: Stack(
         children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Text(
-            "Tickets",
-            style: AppStyles.headLineStyle1,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const AppTicketTabs(
-            firstTab: "Upcoming",
-            secondTab: "Previous",
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          //white ticket
-          Container(
-              padding: const EdgeInsets.only(left: 16),
-              child: TicketView(
-                ticket: ticketList[0],
-                isColor: true,
-              )),
-          const SizedBox(
-            height: 1,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15),
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-            color: AppStyles.ticketColor,
-            child: Column(
-              children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ListView(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Tickets",
+                style: AppStyles.headLineStyle1,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const AppTicketTabs(
+                firstTab: "Upcoming",
+                secondTab: "Previous",
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //white ticket
+              Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: TicketView(
+                    ticket: ticketList[0],
+                    isColor: true,
+                  )),
+              const SizedBox(
+                height: 1,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                color: AppStyles.ticketColor,
+                child: Column(
                   children: [
-                    AppColumnTextLayout(
-                      topText: "Flutter DB",
-                      bottomText: "Passenger",
-                      alignment: CrossAxisAlignment.start,
-                      isColor: true,
-                    ),
-                    AppColumnTextLayout(
-                      topText: "5221 36869",
-                      bottomText: "passport",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const AppLayoutBuilderWidget(
-                  randomDivider: 15,
-                  width: 5,
-                  isColor: false,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AppColumnTextLayout(
-                      topText: "2465 6584948345",
-                      bottomText: "Number of E-ticket",
-                      alignment: CrossAxisAlignment.start,
-                      isColor: true,
-                    ),
-                    AppColumnTextLayout(
-                      topText: "846859",
-                      bottomText: "Booking code",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const AppLayoutBuilderWidget(
-                  randomDivider: 15,
-                  width: 5,
-                  isColor: false,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              AppMedia.visaCard,
-                              scale: 11,
-                            ),
-                            Text(
-                              " *** 2462",
-                              style: AppStyles.headLineStyle2,
-                            )
-                          ],
+                        AppColumnTextLayout(
+                          topText: "Flutter DB",
+                          bottomText: "Passenger",
+                          alignment: CrossAxisAlignment.start,
+                          isColor: true,
                         ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Payment Method",
-                          style: AppStyles.headLineStyle4,
+                        AppColumnTextLayout(
+                          topText: "5221 36869",
+                          bottomText: "passport",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: true,
                         )
                       ],
                     ),
-                    const AppColumnTextLayout(
-                      topText: "\$249.99",
-                      bottomText: "Price",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
-                    )
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const AppLayoutBuilderWidget(
+                      randomDivider: 15,
+                      width: 5,
+                      isColor: false,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppColumnTextLayout(
+                          topText: "2465 6584948345",
+                          bottomText: "Number of E-ticket",
+                          alignment: CrossAxisAlignment.start,
+                          isColor: true,
+                        ),
+                        AppColumnTextLayout(
+                          topText: "846859",
+                          bottomText: "Booking code",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: true,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const AppLayoutBuilderWidget(
+                      randomDivider: 15,
+                      width: 5,
+                      isColor: false,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  AppMedia.visaCard,
+                                  scale: 11,
+                                ),
+                                Text(
+                                  " *** 2462",
+                                  style: AppStyles.headLineStyle2,
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Payment Method",
+                              style: AppStyles.headLineStyle4,
+                            )
+                          ],
+                        ),
+                        const AppColumnTextLayout(
+                          topText: "\$249.99",
+                          bottomText: "Price",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: true,
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 1,),
-          // Bottom of the ticket detail section
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            padding: EdgeInsets.symmetric(vertical: 20),
-            decoration: BoxDecoration(
-                color: AppStyles.ticketColor,
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(21),
-                    bottomLeft: Radius.circular(21))),
-            child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: BarcodeWidget(
-                  height: 70,
-                  barcode: Barcode.code128(),
-                  data: 'https://port-one-opal.vercel.app/',
-                  drawText: false,
-                  color: AppStyles.textColor,
-                  width: double.infinity,
+              ),
+              const SizedBox(height: 1,),
+              // Bottom of the ticket detail section
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                padding: EdgeInsets.symmetric(vertical: 20),
+                decoration: BoxDecoration(
+                    color: AppStyles.ticketColor,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(21),
+                        bottomLeft: Radius.circular(21))),
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: BarcodeWidget(
+                      height: 70,
+                      barcode: Barcode.code128(),
+                      data: 'https://port-one-opal.vercel.app/',
+                      drawText: false,
+                      color: AppStyles.textColor,
+                      width: double.infinity,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 20,),
+              //colorful ticket
+              Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: TicketView(
+                    ticket: ticketList[0],
+                  )),
+            ],
           ),
-          SizedBox(height: 20,),
-          //colorful ticket
-          Container(
-              padding: const EdgeInsets.only(left: 16),
-              child: TicketView(
-                ticket: ticketList[0],
-              )),
+          TicketPositionCircle(pos: true,),
+          TicketPositionCircle(pos: null,),
         ],
       ),
     );
