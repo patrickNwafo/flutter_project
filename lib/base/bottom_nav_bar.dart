e27773b0aacea4175959bb308f13350d3ec43803
ricky <patrickchinwafor@gmail.com>
2024-12-16 17:18:52 +0100

    Injecting GetX

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,19 +1,19 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/controller/bottom_nav_controller.dart';
 import 'package:ticket_app/screens/home/home_screen.dart';
 import 'package:ticket_app/screens/search/search_screen.dart';
 
 import '../screens/profile/profile.dart';
 import '../screens/tickets/ticket_screen.dart';
+import 'package:get/get.dart';
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+class BottomNavBar extends StatelessWidget {
+   BottomNavBar({super.key});
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
+   //Dependency injection
+   final BottomNavController controller = Get.put(BottomNavController());
 
-class _BottomNavBarState extends State<BottomNavBar> {
   //list is iterated using index
   final appScreens = [
     const HomeScreen(),
@@ -22,43 +22,39 @@ class _BottomNavBarState extends State<BottomNavBar> {
     const ProfileScreen(),
   ];
 
-  //Change our index for bottom Navbar
-  int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: appScreens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-          selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-                label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-                label: "Search"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
-                label: "Tickets"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-                label: "Profile"),
-          ]),
-    );
+    print("My tab val ${controller.selectedIndex.value}");
+    return Obx((){
+      return Scaffold(
+        body: appScreens[controller.selectedIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: controller.selectedIndex.value,
+            onTap: controller.onItemTapped,
+            selectedItemColor: Colors.blueGrey,
+            unselectedItemColor: const Color(0xFF526400),
+            showSelectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                  label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                  label: "Search"),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                  label: "Tickets"),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                  label: "Profile"),
+            ]),
+      );
+    });
   }
 }
